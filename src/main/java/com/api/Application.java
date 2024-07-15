1c747055560d5a9357b7f926db5770d62ad794ad
Juliano Hiroi <julianohiroi@gmail.com>
2024-07-15 17:25:19 -0300

    fix problem with .env in railways

--- src/main/java/com/api/Application.java
+++ src/main/java/com/api/Application.java
@@ -3,16 +3,10 @@ package com.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
 		SpringApplication.run(Application.class, args);
 	}
 
